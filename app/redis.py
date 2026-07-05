e5998fe8011a0b503bdea8f213766c93a21a1ba9
daniel tega <dantegaefe@gmail.com>
2026-07-05 01:05:15 +0100

    added allowed  endpoint

--- app/redis.py
+++ app/redis.py
@@ -6,10 +6,12 @@ load_dotenv()
 
     
 REDIS_PORT = os.getenv("REDIS_PORT")
+REDIS_PASSWORD = os.getenv("REDIS_PORT")
 
 redis_client = redis.Redis(
     host='localhost',
     port=REDIS_PORT,
+    password=REDIS_PASSWORD,
     db=int(0),
     decode_responses=True
 )
